9a1d78f6330f829d6410ae3d2b76f0844728a03f
knawattranakul <knawattranakul@splunk.com>
2021-06-30 17:51:56 -0700

    Doc: Update Copyright.

--- symanteccas_connector.py
+++ symanteccas_connector.py
@@ -1,15 +1,9 @@
 # File: symanteccas/symanteccas_connector.py
+# Copyright (c) 2018-2021 Splunk Inc.
 #
-# Copyright (c) Phantom Cyber Corporation, 2016-2017
+# SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
+# without a valid written license from Splunk Inc. is PROHIBITED.
 #
-# This unpublished material is proprietary to Phantom Cyber.
-# All rights reserved. The methods and
-# techniques described herein are considered trade secrets
-# and/or confidential. Reproduction or distribution, in whole
-# or in part, is forbidden except by express written permission
-# of Phantom Cyber Corporation.
-#
-# --
 
 # Phantom imports
 import phantom.app as phantom
--- symanteccas_consts.py
+++ symanteccas_consts.py
@@ -1,16 +1,10 @@
-# --
 # File: symanteccas/symanteccas_consts.py
+# Copyright (c) 2018-2021 Splunk Inc.
 #
-# Copyright (c) Phantom Cyber Corporation, 2016-2017
+# SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
+# without a valid written license from Splunk Inc. is PROHIBITED.
 #
-# This unpublished material is proprietary to Phantom Cyber.
-# All rights reserved. The methods and
-# techniques described herein are considered trade secrets
-# and/or confidential. Reproduction or distribution, in whole
-# or in part, is forbidden except by express written permission
-# of Phantom Cyber Corporation.
-#
-# --
+
 SYMANTECCAS_JSON_API_KEY = 'api_key'
 SYMANTECCAS_JSON_URL = 'url'
 SYMANTECCAS_JSON_VERIFY_SERVER_CERT = 'verify_server_cert'
